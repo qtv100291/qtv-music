1cb0e74843a4e2bff0a856842e9867c456f885d2
qtv91 <quangtuanvu1991@gmail.com>
2020-08-10 10:46:43 +0700

    Updates

---
+++ precache-manifest.67492c47ec4615e8ecd7fc84f11a1969.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ee10adbb02216e2579126675e4a523",
+    "url": "/qtv-music/index.html"
+  },
+  {
+    "revision": "1096ddafd6bd6b7b0903",
+    "url": "/qtv-music/static/css/2.63d4361e.chunk.css"
+  },
+  {
+    "revision": "a569be45ddc3e85c3df4",
+    "url": "/qtv-music/static/css/main.2f212ab0.chunk.css"
+  },
+  {
+    "revision": "1096ddafd6bd6b7b0903",
+    "url": "/qtv-music/static/js/2.66167d77.chunk.js"
+  },
+  {
+    "revision": "ccb07d3f36c83d118d798038f894bf02",
+    "url": "/qtv-music/static/js/2.66167d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a569be45ddc3e85c3df4",
+    "url": "/qtv-music/static/js/main.c7bce4e9.chunk.js"
+  },
+  {
+    "revision": "b7ca8df91ed16df36e71",
+    "url": "/qtv-music/static/js/runtime-main.2fb9a8dc.js"
+  },
+  {
+    "revision": "d4f74f6db1aacaa75db345f5caf9df73",
+    "url": "/qtv-music/static/media/Chester-Bennington.d4f74f6d.jpg"
+  },
+  {
+    "revision": "27df0dfb2b0e0eb3a6a147811d5566eb",
+    "url": "/qtv-music/static/media/banner.27df0dfb.jpg"
+  },
+  {
+    "revision": "ba78527856c9be34b304f21ff2b03a18",
+    "url": "/qtv-music/static/media/banner2.ba785278.jpg"
+  },
+  {
+    "revision": "c12aebd8363e0860c726538c21402448",
+    "url": "/qtv-music/static/media/banner3.c12aebd8.jpg"
+  },
+  {
+    "revision": "731b0b12d1d89c2fbe68c4473fa65b88",
+    "url": "/qtv-music/static/media/broken-guitar.731b0b12.png"
+  },
+  {
+    "revision": "5bba35bcaa1d02979bede2cb678a1a08",
+    "url": "/qtv-music/static/media/buc-tuong-1.5bba35bc.jpg"
+  },
+  {
+    "revision": "6cfa1579f4176e21d66dbe53dbdcb562",
+    "url": "/qtv-music/static/media/dragonforce-1.6cfa1579.jpg"
+  },
+  {
+    "revision": "45fff47f9c7127f7b633ef955e5a8f3d",
+    "url": "/qtv-music/static/media/finland.45fff47f.jpeg"
+  },
+  {
+    "revision": "93540fc56f17190fc30407e04adb38eb",
+    "url": "/qtv-music/static/media/hac-san-1.93540fc5.jpg"
+  },
+  {
+    "revision": "078bba54c76daac38cfb40ad36bd856f",
+    "url": "/qtv-music/static/media/linkin-park.078bba54.jpg"
+  },
+  {
+    "revision": "104b15544a40a98e74be382dcf405889",
+    "url": "/qtv-music/static/media/loading-icon.104b1554.gif"
+  },
+  {
+    "revision": "71626c8669f19a70861e7c571f2baa47",
+    "url": "/qtv-music/static/media/manager.71626c86.svg"
+  },
+  {
+    "revision": "59a367c9bf2148d56292ba895b4130a3",
+    "url": "/qtv-music/static/media/metallica.59a367c9.jpg"
+  },
+  {
+    "revision": "ef99d9c7297615f57b4ef558a035b4ec",
+    "url": "/qtv-music/static/media/night-wish-1.ef99d9c7.jpg"
+  },
+  {
+    "revision": "7a461344910dc4feb4bfb2e0b3df9a9a",
+    "url": "/qtv-music/static/media/thrash-metal.7a461344.jpg"
+  },
+  {
+    "revision": "dfea5317a566e95fed385fcc99a26e30",
+    "url": "/qtv-music/static/media/unlimited.dfea5317.jpg"
+  },
+  {
+    "revision": "c99d85643b98d23ac60116b67c03bf72",
+    "url": "/qtv-music/static/media/up-arrow.c99d8564.svg"
+  }
+]);
